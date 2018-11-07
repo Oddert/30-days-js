bdbf95da2e6849fa783d3287b343ceec66dc1e5d
Robyn <oddert@gmail.com>
2018-11-07 22:02:10 +0000

    added day 8

--- 7. Array Cardio Second/public/js/index.js
+++ 7. Array Cardio Second/public/js/index.js
@@ -14,3 +14,39 @@ const comments = [
   { text: 'Ramen is my fav food ever', id: 123523 },
   { text: 'Nice Nice Nice!', id: 542328 },
 ]
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  function postAnswers (target, data) {
+    const elem = document.getElementById(target)
+    elem.append(data)
+  }
+
+  // 1) Array.prototype.some() - Is at least one person 19?
+  let one = people.some(each => each.year >= 2018-19)
+  postAnswers('one', JSON.stringify(one))
+  console.log('1) Someone is 19 or older? ', one)
+
+  // 2) Array.prototype.every() - Is everyone 19?
+  let two = people.every(each => each.year === 2018-19)
+  postAnswers('two', JSON.stringify(two))
+  console.log('2) Is everyone 19 or older?', two)
+
+  // 3) Array.prototype.find() Find comment id: 823423
+  let three = comments.find(each => each.id == 823423)
+  postAnswers('three', JSON.stringify(three))
+  console.log('3) Comment id: 823423', three)
+
+  // 4) Array.prototype.findIndex() Find comment id: 823423 and delete
+  let fourIdx = comments.findIndex(each => each.id == 823423)
+  let four = [...comments.splice(0, fourIdx), ...comments.splice(fourIdx + 1)]
+  postAnswers('four', JSON.stringify(four))
+  console.log('4) Array minus comment id: 823423...')
+  console.table(four)
+
+
+
+
+
+})
