5dd06b6d80129fc5b4f6c36b6f9d170e20ecba47
Robyn <oddert@gmail.com>
2018-11-11 12:01:19 +0000

    added shift check functionality

--- 10. Shift to Check Inputs/public/js/index.js
+++ 10. Shift to Check Inputs/public/js/index.js
@@ -1,10 +1,42 @@
 
+var shift = false
+
+window.addEventListener('keydown', e => {
+  if (e.keyCode === 16) shift = true
+})
+window.addEventListener('keyup', e => {
+  if (e.keyCode === 16) shift = false
+})
 
 const formGroups = document.querySelectorAll('.form-group')
 console.table(formGroups)
 
+var lastIdx = null
+
 function handleClick (e) {
-  console.log(e)
+  const idx = e.target.dataset.idx
+  const willCheck = e.target.checked
+  // console.log(`willCheck: ${willCheck}`)
+  // console.log(idx, lastIdx, shift)
+  if (shift && lastIdx !== idx) {
+    let direction = idx > lastIdx
+    let range = []
+    if (direction) {
+      for (let i=Number(lastIdx); i<=Number(idx); i++) {
+        range.push(i)
+      }
+    } else {
+      for (let i=Number(idx); i<=Number(lastIdx); i++) {
+        range.push(i)
+      }
+    }
+    // console.log(range)
+    for (let i=0; i<range.length; i++) {
+      let each = range[i]
+      let elem = document.querySelector(`[data-idx='${each}']`).checked = willCheck
+    }
+  }
+  lastIdx = idx
 }
 
-formGroups.forEach(each => each.addEventListener('change', handleClick(each)))
+formGroups.forEach(each => each.addEventListener('change', handleClick))
