58d3beed7c09027ee07935d857895e4d9466e4d9
Robyn <oddert@gmail.com>
2018-11-14 22:55:33 +0000

    added last of play functionality and copied some css from the initial files to show the progress bar

--- 11. HTML Video Player/public/js/index.js
+++ 11. HTML Video Player/public/js/index.js
@@ -8,6 +8,8 @@ const toggle = player.querySelector('.toggle')
 const skipButtons = player.querySelectorAll('[data-skip]')
 const ranges = player.querySelectorAll('.player-slider')
 
+let mousedown = false
+
 function togglePlay () {
   if (video.paused) video.play()
   else video.pause()
@@ -22,6 +24,22 @@ function skip () {
   video.currentTime += parseFloat(this.dataset.skip)
 }
 
+function handleRangeChange () {
+  video[this.name] = this.value
+}
+
+function handleProgress () {
+  const percent = (video.currentTime / video.duration) * 100
+  progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub (e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
+  video.currentTime = scrubTime
+}
+
+document.addEventListener('mousedown', () => mousedown = true)
+document.addEventListener('mouseup', () => mousedown = false)
 
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
@@ -32,3 +50,13 @@ toggle.addEventListener('click', togglePlay)
 skipButtons.forEach(each =>
   each.addEventListener('click', skip)
 )
+
+ranges.forEach(each => {
+  each.addEventListener('change', handleRangeChange)
+  // each.addEventListener('mousemove', e => mousedown && handleRangeChange(e))
+})
+
+video.addEventListener('timeupdate', handleProgress)
+
+progress.addEventListener('click', scrub)
+progress.addEventListener('mousemove', e => mousedown && scrub(e))
