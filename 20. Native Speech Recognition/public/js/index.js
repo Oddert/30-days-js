252838a81e6f6d3dc54e7146617dc40a502ac058
Robyn <oddert@gmail.com>
2018-11-25 15:51:31 +0000

    added a suit of animations and responces for the bot, no makes weather api calls

--- 20. Native Speech Recognition/public/js/index.js
+++ 20. Native Speech Recognition/public/js/index.js
@@ -12,12 +12,46 @@ words.appendChild(p)
 
 recognition.addEventListener('speechstart', () => hat.classList.add('lightup'))
 
+function debounce (func, wait=0, immediate=true) {
+  console.log('debouncing')
+  var timeout;
+  return function () {
+    var context = this, args = arguments
+    var later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    var callNow = immediate && !timeout
+    clearTimeout (timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+}
+
 recognition.addEventListener('result', e => {
   console.log(e)
   const transcript = Array.from(e.results)
     .map(res => res[0])
     .map(res => res.transcript)
     .join('')
+
+  if (transcript.includes('love you')) love()
+  if (transcript.includes('weather')) getWeather()
+  if (transcript.includes('download')) voiceDownload()
+  if (
+    transcript.includes('good bot') ||
+    transcript.includes('well done') ||
+    transcript.includes('I like you') ||
+    transcript.includes('I like this') ||
+    transcript.includes('so good') ||
+    transcript.includes('clever bot')
+  ) thanks()
+  if (
+    transcript.includes('good job') ||
+    transcript.includes('nice') ||
+    transcript.includes('thanks') ||
+    transcript.includes('thank you')
+  ) welcome()
 
   p.textContent = transcript
   if (e.results[0].isFinal) {
@@ -66,6 +100,10 @@ const mouth = document.querySelector('.mouth')
 
 let canBlink = true
 let canWrite = true
+let talking = false
+
+let animateDebounceActive = false
+
 
 function blink () {
   leftEye.setAttribute('src', '/img/eye closed.png')
@@ -78,44 +116,63 @@ function blink () {
 
 async function randomBlink () {
   setTimeout (() => {
-    if (canBlink) Math.floor(Math.random()*5) === 1 ? blink() : wink()
+    if (canBlink && !talking) Math.floor(Math.random()*5) === 1 ? blink() : wink()
     randomBlink()
   }, Math.floor(Math.random()*20000)+5000)
 }
 setTimeout(randomBlink, 2000)
 
 
-async function surprise () {
-  canBlink = false
-  leftEye.setAttribute('src', '/img/eye surprise left.png')
-  rightEye.setAttribute('src', '/img/eye surprise right.png')
-  mouth.setAttribute('src', '/img/eye default.png')
-  mouth.style.height = '25px'
-  mouth.style.width = '25px'
-  setTimeout(() => {
+function toggleSurprise (surpriseState) {
+  if (surpriseState) {
+    leftEye.setAttribute('src', '/img/eye surprise left.png')
+    rightEye.setAttribute('src', '/img/eye surprise right.png')
+    mouth.setAttribute('src', '/img/eye default.png')
+    mouth.style.height = '25px'
+    mouth.style.width = '25px'
+  } else {
     leftEye.setAttribute('src', '/img/eye default.png')
     rightEye.setAttribute('src', '/img/eye default.png')
     mouth.setAttribute('src', '/img/mouth default.png')
     mouth.style.height = null
     mouth.style.width = null
+  }
+}
+
+async function surprise () {
+  canBlink = false
+  toggleSurprise(true)
+  setTimeout(() => {
+    toggleSurprise(false)
     canBlink = true
   }, Math.floor(Math.random()*5000))
 }
 
+
+function toggleWink (winkState) {
+  if (winkState) {
+    leftEye.setAttribute('src', '/img/eye closed.png')
+    rightEye.setAttribute('src', '/img/eye default.png')
+    mouth.setAttribute('src', '/img/mouth smirk.png')
+  } else {
+    leftEye.setAttribute('src', '/img/eye default.png')
+    rightEye.setAttribute('src', '/img/eye default.png')
+    mouth.setAttribute('src', '/img/mouth default.png')
+  }
+}
+
 async function wink () {
   canBlink = false
-  leftEye.setAttribute('src', '/img/eye closed.png')
-  mouth.setAttribute('src', '/img/mouth smirk.png')
+  toggleWink(true)
   setTimeout (() => {
-    leftEye.setAttribute('src', '/img/eye default.png')
-    mouth.setAttribute('src', '/img/mouth default.png')
+    toggleWink(false)
     canBlink = true
   }, 500)
 }
 // setTimeout(canBlink ? wink : () => {}, 3000)
 
 
-let talking = false
+
 function toggleTalking (talkState) {
   if (talkState) {
     canBlink = false
@@ -134,7 +191,7 @@ function toggleTalking (talkState) {
   }
 }
 
-async function write (text) {
+async function write (text, fast) {
   toggleTalking(true)
   const dialogue = document.querySelector('.dialogue')
   const elem = document.createElement('P')
@@ -143,11 +200,119 @@ async function write (text) {
   function output () {
     elem.textContent = text.substring(0,i)
     i ++
-    if (i <= text.length) setTimeout(output, Math.floor(Math.random()*200))
+    if (i <= text.length) setTimeout(output, Math.floor(Math.random()*(fast ? 20 : 150)))
     else toggleTalking(false)
   }
   output()
 }
 
-write("Here is a test message for odd bot to output")
-setTimeout(() => write("and here is another string after "), 15000)
+
+function heart (heartState) {
+  if (heartState) {
+    leftEye.setAttribute('src', '/img/eye heart.png')
+    rightEye.setAttribute('src', '/img/eye heart.png')
+  } else {
+    leftEye.setAttribute('src', '/img/eye default.png')
+    rightEye.setAttribute('src', '/img/eye default.png')
+  }
+}
+
+function love () {
+  if (!animateDebounceActive) {
+    animateDebounceActive = true
+    heart(true)
+    write ("You love me?! I love you back friend! 💖", true)
+    setTimeout(() => {
+      wink()
+      animateDebounceActive = false
+    }, 4200)
+  }
+}
+
+function thanks () {
+  if (!animateDebounceActive) {
+    animateDebounceActive = true
+    heart(true)
+    write ("Thank you for your kindness!", true)
+    setTimeout(() => {
+      leftEye.setAttribute('src', '/img/eye default.png')
+      rightEye.setAttribute('src', '/img/eye default.png')
+      mouth.setAttribute('src', '/img/mouth default.png')
+      animateDebounceActive = false
+    }, 3600)
+  }
+}
+
+function welcome () {
+  if (!animateDebounceActive) {
+    animateDebounceActive = true
+    heart(true)
+    write ("You're very welcome! Any time", true)
+    setTimeout(() => {
+      leftEye.setAttribute('src', '/img/eye default.png')
+      rightEye.setAttribute('src', '/img/eye default.png')
+      mouth.setAttribute('src', '/img/mouth default.png')
+      animateDebounceActive = false
+    }, 3600)
+  }
+}
+
+function toggleRadar (radarState) {
+  if (radarState) {
+    hat.classList.add('talking')
+  } else {
+    hat.classList.remove('talking')
+  }
+}
+
+const convTemp = temp => Math.round(temp-273.15)
+
+function getWeather () {
+  if (!animateDebounceActive) {
+    animateDebounceActive = true
+    canBlink = false
+    toggleRadar(true)
+    navigator.geolocation.getCurrentPosition(position => {
+      let weather = `http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&APPID=a48e5c0a6ce401edc508797ffd0f530c`
+      fetch(weather)
+        .then(res => res.json())
+        .then(res => {
+          console.log(res)
+          let lineOne = `The current weather in ${res.name} is ${res.weather[0].main} at ${convTemp(res.main.temp)} degrees, with highs of ${convTemp(res.main.temp_max)} and lows of ${convTemp(res.main.temp_min)}`
+          write(lineOne, true)
+          setTimeout(() => {
+            toggleRadar()
+            animateDebounceActive = false
+            canBlink = true
+          }, 5000)
+        })
+    })
+  }
+}
+
+
+
+function voiceDownload () {
+  // if (!animateDebounceActive) {
+  //   animateDebounceActive = true
+  //   write("I heard you say \"download\". Do you want to download what you have writen? (Say YES or NO)")
+  //   setTimeout(() => {animateDebounceActive=false}, 10000)
+  // }
+}
+
+
+
+// write("Here is a test message for odd bot to output")
+// setTimeout(() => write("and here is another string after "), 2000)
+// setTimeout(() => write("and here is another string after we need a really long sentance to properly test how the animations override. Do you think there is an easier wya to do this? Maybe I should go backto using some lorem ipsum text. Feasably none of the genreated content will ever be longer than this..."), 15000)
+write("Hello! I'm OddBot, the @Oddert robot. I'll write down your speech for you to download!", true)
+navigator.webkitGetUserMedia({ audio: true }
+    , () => {console.log('User has allowed microphone')}
+    , () => {
+      console.log('User has disabled microphone')
+      write("Uh oh! You need to enable a microphone for this to work, am no psychic...")
+    })
+
+setTimeout(() => {
+  write("")
+}, 5000)
