dcde1a9113a20c5a52f95396553a92af541f5126
Robyn <oddert@gmail.com>
2018-12-02 23:49:20 +0000

    added final styling to the past layout change

--- 17. Sort without Articles/public/js/index.js
+++ 17. Sort without Articles/public/js/index.js
@@ -1,7 +1,7 @@
 
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog']
 
-document.querySelector('.input').innerHTML = bands.map((each, idx) => `<li>${idx}) ${each}</li>`).join('')
+document.querySelector('.input-list').innerHTML = bands.map((each, idx) => `<li>${idx}) ${each}</li>`).join('')
 
 const strip = name => name.replace(/^(a |the |an )/i, '').trim()
 
@@ -9,4 +9,4 @@ const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1)
 
 console.log(sortedBands)
 
-document.querySelector('.bands').innerHTML = sortedBands.map((each, idx) => `<li>${idx}) ${each}</li>`).join('')
+document.querySelector('.bands-list').innerHTML = sortedBands.map((each, idx) => `<li>${idx}) ${each}</li>`).join('')
